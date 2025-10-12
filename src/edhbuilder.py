6aa0bf74b6df181acbaeea19258e21ae91bb6b05
jkk290 <jared@kuromoto.dev>
2025-10-11 16:40:42 -1000

    Add view_decks function
    view_decks function creates decks directory and list all decks found in the directory

--- src/edhbuilder.py
+++ src/edhbuilder.py
@@ -1,11 +1,12 @@
 from .createDeck import create_deck
+from .viewDecks import view_decks
 
 def edhbuilder():
     print("++++++++++++++++++++++")
     print("Welcome to EDH Builder")
     print("++++++++++++++++++++++")
     while (True):
-        print("=============================")
+        print("\n=============================")
         print("1. Start building a new deck\n2. Edit an existing deck\n3. View all decks")
         print("=============================")
         menu_selection = input("Enter a selection (q to quit): ")
@@ -14,6 +15,8 @@ def edhbuilder():
                 break
             case "1":
                 create_deck()
+            case "3":
+                view_decks()
             case _:
                 print("Invalid selection")
     print("Goodbye!")
