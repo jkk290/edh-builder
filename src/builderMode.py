c08ea2ca057d6bcf5a93e3a13aecf8ca134168ae
jkk290 <jared@kuromoto.dev>
2025-10-15 10:11:43 -1000

    Update buildermode for adding cards to deck list

--- src/builderMode.py
+++ src/builderMode.py
@@ -1,23 +1,39 @@
+import re
 from .utils.deckHelper import get_deck_content
+
+def print_deck_content(deck_content):
+    print("Current deck list")
+    print("+++++++++++++++++\n")
+    for line in deck_content:
+        print(f"{line} - {deck_content[line]}")
+    print("====================\n")
 
 def builder_mode(deck_name):
     print("==================")
     print("=  Builder Mode  =")
-    print("==================")
+    print("==================\n")
 
     try:
         deck_content = get_deck_content(deck_name)
-        print("+++++++++++++++++")
-        print("Current deck list")
-        print("+++++++++++++++++\n")
-        for line in deck_content:
-            print(f"{line} - {deck_content[line]}")
-        print("====================\n")
     except Exception:
-        print("error getting deck")
+        print("Error getting deck")
+
+    print_deck_content(deck_content)
 
     while True:
         builder_input = input("(builder)> ")
-        if builder_input == "q":
-            return
+        match builder_input:
+            case "q":
+                return
+            case "show deck":
+                print_deck_content(deck_content)
+            case s if s.startswith("add "):
+                splitted_input = builder_input.split(" ", 1)
+                has_qty = re.search("[-][0123456789]+$", splitted_input[1])
+                if has_qty:
+                    final_input = splitted_input[1].split(" -")
+                    deck_content[final_input[0]] = final_input[1]
+                else:
+                    deck_content[splitted_input[1]] = 1
+                
         
