90c6eb4ca1296d4320bdbf50e93120ccbdac2f62
jkk290 <jared@kuromoto.dev>
2025-10-16 16:28:08 -1000

    Update the deck creation process to check for background or partner

--- src/createDeck.py
+++ src/createDeck.py
@@ -16,7 +16,28 @@ def create_deck():
             return
         elif deck_commander == "":
             print("Deck must have a commander")
-    formatted_commander = [{"card_name": deck_commander, "type": "commander", "qty": 1}]
+    background_or_partner = True
+    deck_background = None
+    deck_partner = None
+    while background_or_partner:
+        print("Will your deck have a background or partner?\n1. Background\n2. Partner\n3. None")
+        response = input("Please enter 1-3 (q to quit back to menu) ")
+        match response:
+            case "1":
+                deck_background = input("Enter the background card name: ")
+                break
+            case "2":
+                deck_partner = input("Enter the partner card name: ")
+                break
+            case "3":
+                background_or_partner = False
+            case _:
+                print("Invalid selection")
+    deck_content = [{"card_name": deck_commander, "type": "commander", "qty": 1}]
+    if background_or_partner and deck_background:
+        deck_content.append({"card_name": deck_background, "type": "background", "qty": 1})
+    elif background_or_partner and deck_partner:
+        deck_content.append({"card_name": deck_partner, "type": "partner", "qty": 1})
     create_deck_file(deck_name)
-    write_deck_file(deck_name, formatted_commander)
+    write_deck_file(deck_name, deck_content)
     builder_mode(deck_name)
